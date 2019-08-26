add670044796f82a6517b3e675905835efe07ba6
Thomas <1779937+parsehex@users.noreply.github.com>
2019-08-26 15:42:31 -0400

    formatting

--- Waterer/ModConfig.cs
+++ Waterer/ModConfig.cs
@@ -1,22 +1,21 @@
+class ModConfig {
+	// key to press
+	public string KeyBind { get; set; } = "K";
 
-	class ModConfig {
-		// key to press
-		public string KeyBind { get; set; } = "K";
+	// how much to charge for each watering
+	// the final cost is rounded to the nearest whole number
+	public float Price { get; set; } = 0.5f;
 
-		// how much to charge for each watering
-		// the final cost is rounded to the nearest whole number
-		public float Price { get; set; } = 0.5f;
+	// whether to water all Hoed Dirt tiles
+	// true = water all Hoed Dirt tiles
+	// false = only water Hoed Dirt where a crop is planted
+	public bool WaterAll { get; set; } = false;
 
-		// whether to water all Hoed Dirt tiles
-		// true = water all Hoed Dirt tiles
-		// false = only water Hoed Dirt where a crop is planted
-		public bool WaterAll { get; set; } = false;
+	// whether to show a message in-game after the deed is done
+	public bool Message { get; set; } = true;
 
-		// whether to show a message in-game after the deed is done
-		public bool Message { get; set; } = true;
+	// whether to skip watering fully-grown crops
+	public bool SkipFullyGrown { get; set; } = true;
 
-		// whether to skip watering fully-grown crops
-		public bool SkipFullyGrown { get; set; } = true;
-
-		public bool debug {get;set;} = false;
-	}
+	public bool debug {get;set;} = false;
+}
